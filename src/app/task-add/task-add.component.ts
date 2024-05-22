4f1b84f2b039f3ed1e436297cfd6f42c786c2881
celene <cegumarinve@ittepic.edu.mx>
2024-05-22 01:05:13 -0700

    validacion de echa

--- src/app/task-add/task-add.component.ts
+++ src/app/task-add/task-add.component.ts
@@ -15,6 +15,8 @@ export class TaskAddComponent {
   constructor(private taskService: TaskService, private router: Router) { }
 
   addTask(): void {
+    console.log('Adding task:', this.newTask, this.isValidTask());
+    
     if (this.isValidTask()) {
       this.taskService.getTasks().subscribe(tasks => {
         const lastTaskId = Number(tasks[tasks.length - 1]?.id || 0);
@@ -33,11 +35,48 @@ export class TaskAddComponent {
           }
         );
       });
+    } else {
+      alert(this.errorMessage);
     }
   }
   
 
   private isValidTask(): boolean {
-    return this.newTask.title.trim() !== '' && this.newTask.description.trim() !== '' && this.newTask.deadline.trim() !== '';
+    var arreglo = this.newTask.deadline.trim().split('-');
+    const currentDate = new Date();
+    var today = currentDate.toISOString().split('T')[0];
+    var todayArreglo = today.split('-');
+    var validDate = true;
+
+    if(arreglo[0] < todayArreglo[0] ){
+      this.errorMessage = 'The year is invalid';
+      validDate = false;
+    } else {
+      if(arreglo[0] == todayArreglo[0]){
+        if(arreglo[1] < todayArreglo[1]){
+          this.errorMessage = 'The month is invalid';
+          validDate = false;
+        } else {
+          if(arreglo[1] == todayArreglo[1]){
+            if(arreglo[2] < todayArreglo[2]){
+              this.errorMessage = 'The day is invalid';
+              validDate = false;
+            }
+          }
+        }
+      }
+    }
+
+    if(arreglo[1] < todayArreglo[1]){
+      this.errorMessage = 'The month is invalid';
+      validDate = false;
+    }
+
+    if(arreglo[2] < todayArreglo[2]){
+      this.errorMessage = 'The day is invalid';
+      validDate = false;
+    }
+    
+    return this.newTask.title.trim() !== '' && this.newTask.description.trim() !== '' && (this.newTask.deadline.trim() !== '' && validDate);
   }
 }
